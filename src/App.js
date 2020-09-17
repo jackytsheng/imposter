fff3d466c5b53fc4bc4f6cd9cf42acfc1f8b3c18
jackytsheng <jackytsheng@gmail.com>
2020-09-17 23:31:16 +1000

    initialise ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Center = styled.div`
+  display:flex;
+  position:fixed;
+  top:0;
+  bottom:0;
+  left:0;
+  right:0;
+  justify-content:center;
+  align-items:center;
+`
+const CenterSquare = styled.div`
+  width:300px;
+  height:300px;
+  border: solid 1px red;
+  display:flex;
+  flex-direction:column;
+  flex-wrap:wrap;
+  justify-content:center;
+  align-items:center;
+`
+
+const PLAYER_COUNT = 10;
+const PlayerSlot = styled.div`
+  width:100px;
+  height:50px;
+  border: 1px blue solid;
+`
+
+const generatePlayer = ()=>{
+  let renderResult =[];
+
+  for (let i = 0; i<PLAYER_COUNT; i++ ){
+    renderResult.push(
+      <PlayerSlot key = {"player_slot" + i}/>
+    )
+  }
 
 
-function App() {
-  return (
-    <div>
-          Learn React
-    </div>
-  );
+  return renderResult;
 }
 
-export default App;
+export default () => 
+<Center>
+  <CenterSquare>
+    {generatePlayer()}
+  </CenterSquare>
+</Center>
